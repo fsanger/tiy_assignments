03051d657e9c437a81418e023a37c6b09391d4b1
fsanger <fxsanger@gmail.com>
2015-07-25 09:06:26 -0500

    Move GoodReads Style Guide from day 44 to top level of tiy_assignments, update GoodReads Comp_laytoutsV3.2

---
+++ GoodReads Style Guide/js/file.js
@@ -0,0 +1,64 @@
+
+
+$("#selected_1").on("click",collapse_1);
+function collapse_1(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_1").slideToggle("slow");
+	}	
+}
+
+$("#selected_2").on("click",collapse_2);
+function collapse_2(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_2").slideToggle("slow");
+	}	
+}
+
+$("#selected_3").on("click",collapse_3);
+function collapse_3(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_3").slideToggle("slow");
+	}	
+}
+
+$("#selected_4").on("click",collapse_4);
+function collapse_4(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_4").slideToggle("slow");
+	}	
+}
+
+$("#selected_5").on("click",collapse_5);
+function collapse_5(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_5").slideToggle("slow");
+	}	
+}
+
+$("#selected_6").on("click",collapse_6);
+function collapse_6(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_6").slideToggle("slow");
+	}	
+}
+
+$("#selected_7").on("click",collapse_7);
+function collapse_7(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_7").slideToggle("slow");
+	}	
+}
+
+$("#selected_8").on("click",collapse_8);
+function collapse_8(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_8").slideToggle("slow");
+	}	
+}
+
+$("#selected_9").on("click",collapse_9);
+function collapse_9(){
+	if ($(window).width() <= 480){	
+		$("#section-collapse_9").slideToggle("slow");
+	}	
+}
